495ec12e30a4dd1f846a90dd40fde3c153cc53a5
Cata <rc.visan@gmail.com>
2019-02-02 18:09:00 +0200

    asd

---
+++ src/main/java/com/personal/distributedapp/DistributedAppApplication.java
@@ -0,0 +1,14 @@
+package com.personal.distributedapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedAppApplication.class, args);
+    }
+
+}
+
