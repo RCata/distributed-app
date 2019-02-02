72fded90cc65ebbcb9b427614780191427b04263
Cata <rc.visan@gmail.com>
2019-02-02 20:10:59 +0200

    fet

--- src/main/java/com/personal/distributedapp/controller/LoginController.java
+++ src/main/java/com/personal/distributedapp/controller/LoginController.java
@@ -5,11 +5,13 @@ import com.personal.distributedapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class LoginController {
 
     private UserService userService;
@@ -20,10 +22,10 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login (@RequestBody UserDto user){
+    public ResponseEntity login (@RequestBody UserDto user){
         if (userService.userExist(user)){
-            return new ResponseEntity<String>("Success", HttpStatus.OK);
+            return new ResponseEntity(HttpStatus.OK);
         }
-        return new ResponseEntity<String>("No user with those credentials", HttpStatus.BAD_REQUEST);
+        return new ResponseEntity(HttpStatus.BAD_REQUEST);
     }
 }
